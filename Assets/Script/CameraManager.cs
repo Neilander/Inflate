4ee71c5e84a94fd3d9cd8dcd713ebefeb725020f
Neilander <yw6472@nyu.edu>
2026-02-02 00:49:30 -0500

    改了关卡

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -14,6 +14,8 @@ public class CameraManager : MonoBehaviour
     public static float up = 1024f ;
     public static float down = -5f;
 
+    public Vector2 offset;
+
     void Start()
     {
         width = (float)Screen.width / (float)Screen.height;
@@ -22,32 +24,47 @@ public class CameraManager : MonoBehaviour
     void LateUpdate()
     {
         position = transform.position;
+
+        Vector2 target = hero != null
+            ? (Vector2)hero.position + offset
+            : Vector2.zero;
+
+        // ===== X 轴 =====
         if (right - left <= 10f * width)
-            position.x = (left + right) / 2;
-        else if(hero != null)
         {
-            if (position.x - hero.position.x > 2f)
-                position.x = hero.position.x + 2f;
-            else if (position.x - hero.position.x < -2f)
-                position.x = hero.position.x - 2f;
-            if (position.x - left < 5f * width)
-                position.x = left + 5 * width;
-            else if (position.x - right > -5f * width)
-                position.x = right - 5 * width;
+            position.x = (left + right) / 2;
         }
-        if (up - down <= 10f)
-            position.y = (up + down) / 2;
         else if (hero != null)
         {
-            if (position.y - hero.position.y > 3f)
-                position.y = hero.position.y + 3f;
-            else if (position.y - hero.position.y < -1f)
-                position.y = hero.position.y - 1f;
+            if (position.x - target.x > 2f)
+                position.x = target.x + 2f;
+            else if (position.x - target.x < -2f)
+                position.x = target.x - 2f;
+
+            if (position.x - left < 5f * width)
+                position.x = left + 5f * width;
+            else if (position.x - right > -5f * width)
+                position.x = right - 5f * width;
+        }
+
+        // ===== Y 轴 =====
+        if (up - down <= 10f)
+        {
+            position.y = (up + down) / 2;
+        }
+        else if (hero != null)
+        {
+            if (position.y - target.y > 3f)
+                position.y = target.y + 3f;
+            else if (position.y - target.y < -1f)
+                position.y = target.y - 1f;
+
             if (position.y - up > -5f)
                 position.y = up - 5f;
             else if (position.y - down < 5f)
                 position.y = down + 5f;
         }
+
         transform.position = position;
     }
 }
